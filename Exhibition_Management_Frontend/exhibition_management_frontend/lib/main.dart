702f1674cb80a3873120ed9242c9340945b80e13
namraravani <namraravani8@gmail.com>
2024-12-08 11:09:27 +0530

    User Experience Version 1.0 Completed

--- Exhibition_Management_Frontend/exhibition_management_frontend/lib/main.dart
+++ Exhibition_Management_Frontend/exhibition_management_frontend/lib/main.dart
@@ -14,20 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Exhibition Management',
-      home: const ExhibitionDetailsPage(
-        bannerImageUrl: 'assets/Banner_Images/Banner_Image-1.jpeg',
-        venuePhotos: [
-          'assets/Banner_Images/Banner_Image-2.png',
-          'assets/Banner_Images/Banner_Image-2.png',
-          'assets/Banner_Images/Banner_Image-2.png',
-          'assets/Banner_Images/Banner_Image-2.png'
-        ],
-        venueName: "Ponnam hall",
-        address: "A-1003,Statyajeet Sopan,Rajkot",
-        timeDuration: "12 Decemeber to 13 December",
-        numberOfTables: 50,
-        description: "Good Exhibition",
-      ),
+      home: HomePage(),
       theme: ThemeData(
         primarySwatch: Colors.blue, // Set the primary theme color
         visualDensity: VisualDensity.adaptivePlatformDensity,
